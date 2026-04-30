ae169a7b94790b7183fa309ee4a5f424d190f753
waddahbakil <42424111+waddahbakil@users.noreply.github.com>
2026-04-30 17:19:55 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.pharmacypdf"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "com.example.pharmacypdf"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        release { isMinifyEnabled = false }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions { jvmTarget = "1.8" }
+    buildFeatures { viewBinding = true }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.tom_roush:pdfbox-android:2.0.27.0")
+    implementation("de.codecrafters.tableview:tableview:2.9.1")
+}
