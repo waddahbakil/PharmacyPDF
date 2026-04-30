29c6451066c135283a91492110abaa6b674baa5a
waddahbakil <42424111+waddahbakil@users.noreply.github.com>
2026-04-30 22:43:43 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/pharmacypdf/MainActivity.kt
+++ app/src/main/java/com/example/pharmacypdf/MainActivity.kt
@@ -2,14 +2,10 @@ package com.example.pharmacypdf
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        val textView = findViewById<TextView>(R.id.textView)
-        textView.text = "PharmacyPDF App Ready"
     }
 }
