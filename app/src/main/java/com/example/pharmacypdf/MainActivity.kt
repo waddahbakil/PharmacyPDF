ba0ca2b2034f4d4928b0678146bd3f2e5127c5f8
waddahbakil <42424111+waddahbakil@users.noreply.github.com>
2026-04-30 19:33:37 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/pharmacypdf/MainActivity.kt
+++ app/src/main/java/com/example/pharmacypdf/MainActivity.kt
@@ -2,14 +2,14 @@ package com.example.pharmacypdf
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.pharmacypdf.databinding.ActivityMainBinding
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.textView.text = "PharmacyPDF App Ready"
+        setContentView(R.layout.activity_main)
+        
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "PharmacyPDF App Ready"
     }
 }
