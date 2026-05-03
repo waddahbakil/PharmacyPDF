488e36f5db21b9d31cb96da49c4f0d15c81560aa
waddahbakil <42424111+waddahbakil@users.noreply.github.com>
2026-05-03 16:07:23 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,36 +4,37 @@ plugins {
 }
 
 android {
-    namespace = "com.example.pharmacypdf"
+    namespace = "com.waddah.pharmacypdf"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.pharmacypdf"
+        applicationId = "com.waddah.pharmacypdf"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    
     buildFeatures {
         viewBinding = true
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
@@ -44,5 +45,4 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
 }
